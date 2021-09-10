d5f8ccae4a837398e4b2639068ec9cdf06d6697e
VancleyVieira <vancleyskt@gmail.com>
2021-09-10 13:44:29 -0300

    Exercicio 06

---
+++ Exercicio06.js
@@ -0,0 +1,11 @@
+let vetorA = [1,2,3,4,5,6,7,8,9,10]
+let vetorB = [3,4,5,6,7,1,2,9,8,11]
+
+for(let i = 0; i < 10; i++){
+    vetorB[i] = vetorA[i]*2
+
+    console.log(`\nVetor A: ${vetorA[i]}`)
+    console.log(`\nVetor B: ${vetorB[i]}`)
+}
+
+    
