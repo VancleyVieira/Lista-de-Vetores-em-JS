a92363ccab10ee7dbb365456f8165029667d4742
VancleyVieira <vancleyskt@gmail.com>
2021-09-10 13:48:04 -0300

    Adicionando Legenda do Exercicio 06

--- Exercicio06.js
+++ Exercicio06.js
@@ -1,3 +1,6 @@
+/* Criar um  vetor A com  10 elementos inteiros. Construir um vetor B
+     também com 10 elementos onde cada elemento de B é o dobro dos respectivos elementos de A*/
+
 let vetorA = [1,2,3,4,5,6,7,8,9,10]
 let vetorB = [3,4,5,6,7,1,2,9,8,11]
 
